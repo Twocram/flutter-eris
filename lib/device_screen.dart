a13424413bbafe80b4a1e5830de06ea0d220bf5e
Twocram <2002maslovartem@gmail.com>
2022-04-22 14:50:03 +0300

    can add new features for a specific uuid

--- lib/device_screen.dart
+++ lib/device_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_ble/main.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
@@ -22,49 +23,89 @@ class _DeviceScreenState extends State<DeviceScreen> {
   String serviceUuid = '';
   bool isConnected = false;
   bool isCharGetted = false;
+  bool isServicesGetted = false;
 
   final utf8Decoder = utf8.decoder;
   List<String> decodedValues = [];
+  List<String> deviceServices = [];
+  List<String> serviceItemChars = [];
+
   BluetoothDeviceState deviceState = BluetoothDeviceState.disconnected;
 
   StreamSubscription<BluetoothDeviceState>? _stateListener;
 
   @override
   void initState() {
-    _stateListener = widget.device.state.listen((event) {
-      // debugPrint('event: $event');
-      if (deviceState == event) {
-        return;
-      }
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _stateListener = widget.device.state.listen((event) {
+        // debugPrint('event: $event');
+        if (deviceState == event) {
+          return;
+        }
+        setBleConnectionState(event);
+      });
 
-      setBleConnectionState(event);
+      connect();
     });
-
-    connect();
   }
 
-  openWriteDialog() {
-    inputDialog(context);
+  openWriteDialog(String uuid) {
+    inputDialog(context, uuid);
   }
 
-  writeDataInDevice(String userValue) async {
+  writeDataInDevice(String userValue, String deviceUuid) async {
     List<BluetoothService> services = await widget.device.discoverServices();
     for (final BluetoothService s in services) {
-      // print("SERVICE --------------- ${s}");
+      print("SERVICE --------------- ${s.uuid}");
       var chars = s.characteristics;
       for (final BluetoothCharacteristic c in chars) {
-        if (c.properties.write == true) {
+        if (c.uuid.toString() == deviceUuid) {
           try {
             await c.write(utf8.encode(userValue), withoutResponse: true);
             decodedValues.add(userValue);
-            print("ДАННЫЕ УСПЕШНО ДОБАВЛЕНЫ");
           } catch (e) {
-            print("Не удалось добавить данные: ${e}");
+            print("Не удалось добавить данные");
           }
         }
+        // if (c.properties.write == true) {
+        //   try {
+        //     await c.write(utf8.encode(userValue), withoutResponse: true);
+        //     decodedValues.add(userValue);
+        //     print("ДАННЫЕ УСПЕШНО ДОБАВЛЕНЫ");
+        //   } catch (e) {
+        //     print("Не удалось добавить данные: ${e}");
+        //   }
+        // }
       }
     }
   }
+
+  getDeviceServices() async {
+    List<BluetoothService> services = await widget.device.discoverServices();
+    for (final BluetoothService s in services) {
+      var chars = s.characteristics;
+      for (final BluetoothCharacteristic c in chars) {
+        if (c.properties.write) {
+          deviceServices.add(c.uuid.toString());
+        }
+      }
+    }
+    setState(() {
+      isServicesGetted = true;
+    });
+  }
+
+  // getCharItem(String uuid) async {
+  //   List<BluetoothService> services = await widget.device.discoverServices();
+  //   for (final BluetoothService s in services) {
+  //     var chars = s.characteristics;
+  //     for (final BluetoothCharacteristic c in chars) {
+  //       if (c.uuid.toString() == uuid) {
+  //         serviceItemChars.add(c.uuid.toString());
+  //       }
+  //     }
+  //   }
+  // }
 
   getDeviceInfo() async {
     List<BluetoothService> services = await widget.device.discoverServices();
@@ -73,8 +114,6 @@ class _DeviceScreenState extends State<DeviceScreen> {
       var characteristics = service.characteristics;
       for (final BluetoothCharacteristic c in characteristics) {
         if (c.properties.read) {
-          deviceCharUuid = c.uuid.toString();
-          deviceRemoteId = c.serviceUuid.toString();
           List<int> value = await c.read();
           if (value.isNotEmpty) {
             try {
@@ -89,12 +128,6 @@ class _DeviceScreenState extends State<DeviceScreen> {
         }
       }
     }
-
-    decodedValues.add(deviceCharUuid);
-    decodedValues.add(deviceRemoteId);
-    // services[2].characteristics[0].write(
-    //     utf8.encode("MESSAGE FROM ANOTHER DEVICE"),
-    //     withoutResponse: true);
     setState(() {
       isCharGetted = true;
     });
@@ -147,42 +180,42 @@ class _DeviceScreenState extends State<DeviceScreen> {
     setState(() {});
   }
 
-  Future inputDialog(BuildContext context) async {
+  Future inputDialog(BuildContext context, String uuid) async {
     String customValue = "";
-    return showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Добавить данные"),
-            content: new Row(
-              children: <Widget>[
-                new Expanded(
-                    child: new TextField(
-                  autofocus: true,
-                  decoration: new InputDecoration(
-                      labelText: "Название", hintText: 'Новое значение'),
-                  onChanged: (value) {
-                    customValue = value;
-                  },
-                ))
+    Future.delayed(Duration.zero, () async {
+      return showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Добавить данные"),
+              content: new Row(
+                children: <Widget>[
+                  new Expanded(
+                      child: new TextField(
+                    autofocus: true,
+                    decoration: new InputDecoration(
+                        labelText: "Название", hintText: 'Новое значение'),
+                    onChanged: (value) {
+                      customValue = value;
+                    },
+                  ))
+                ],
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () => {Navigator.of(context).pop()},
+                    child: Text("Назад")),
+                FlatButton(
+                    onPressed: () {
+                      writeDataInDevice(customValue, uuid);
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Добавить"))
               ],
-            ),
-            actions: <Widget>[
-              FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text("Назад")),
-              FlatButton(
-                  onPressed: () {
-                    writeDataInDevice(customValue);
-                    Navigator.of(context).pop();
-                  },
-                  child: Text("Добавить"))
-            ],
-          );
-        });
+            );
+          });
+    });
   }
 
   Future<bool> connect() async {
@@ -227,10 +260,57 @@ class _DeviceScreenState extends State<DeviceScreen> {
         : Text('');
   }
 
-  Widget writeDataInDeviceBtn(bool isConnected) {
+  Widget getDeviceServicesBtn(bool isConnected) {
     return isConnected
         ? OutlinedButton(
-            onPressed: openWriteDialog, child: Text("Добавить характеристики"))
+            onPressed: () => getDeviceServices(),
+            child: Text("Узнать services uuid"))
+        : Text('');
+  }
+
+  // Widget writeDataInDeviceBtn(bool isConnected) {
+  //   return isConnected
+  //       ? OutlinedButton(
+  //           onPressed: openWriteDialog, child: Text("Добавить характеристики"))
+  //       : Text('');
+  // }
+
+  // Widget displayServiceChars() {
+  //   return SizedBox(
+  //     child: ListView.builder(
+  //         itemCount: serviceItemChars.length,
+  //         itemBuilder: (context, index) {
+  //           return ListTile(
+  //             title: Text(serviceItemChars[index]),
+  //           );
+  //         }),
+  //   );
+  // }
+
+  Widget displayServices(bool isServicesGetted) {
+    return isServicesGetted
+        ? SizedBox(
+            height: 600,
+            child: ListView.builder(
+                itemCount: deviceServices.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      "${deviceServices[index]}",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        IconButton(
+                            onPressed: () {
+                              openWriteDialog(deviceServices[index].toString());
+                            },
+                            icon: Icon(Icons.more_vert))
+                      ],
+                    ),
+                  );
+                }))
         : Text('');
   }
 
@@ -251,44 +331,59 @@ class _DeviceScreenState extends State<DeviceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          iconSize: 36,
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => {
-            disconnect(),
-            Navigator.pop(context),
-          },
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          leading: IconButton(
+            iconSize: 36,
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => {
+              disconnect(),
+              Navigator.pop(context),
+            },
+          ),
+          title: Text(widget.device.name),
         ),
-        title: Text(widget.device.name),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView(
           children: [
-            Text('$stateText'),
-            OutlinedButton(
-                onPressed: () {
-                  if (deviceState == BluetoothDeviceState.connected) {
-                    disconnect();
-                  } else if (deviceState == BluetoothDeviceState.disconnected) {
-                    connect();
-                  } else {}
-                },
-                child: Text(connectButtonText)),
-            getDeviceInfoBtn(isConnected),
-            writeDataInDeviceBtn(isConnected),
-            Container(
-              padding: EdgeInsets.all(30),
+            Center(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  displayChars(isCharGetted),
+                  Text('$stateText'),
+                  OutlinedButton(
+                      onPressed: () {
+                        if (deviceState == BluetoothDeviceState.connected) {
+                          disconnect();
+                        } else if (deviceState ==
+                            BluetoothDeviceState.disconnected) {
+                          connect();
+                        } else {}
+                      },
+                      child: Text(connectButtonText)),
+                  getDeviceInfoBtn(isConnected),
+                  getDeviceServicesBtn(isConnected),
+                  // writeDataInDeviceBtn(isConnected),
+                  Container(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: [
+                        displayChars(isCharGetted),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 30),
+                    // padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: [
+                        displayServices(isServicesGetted),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
+            ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
